b350de1e3952d88d52d311042a205c286ce8a637
alexsmith922 <10asmith@gmail.com>
2025-11-01 22:53:16 -0500

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Alex Smith on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
