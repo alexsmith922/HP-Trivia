d5b86cba1502664fe238d776dba7ca941c245ae7
alexsmith922 <10asmith@gmail.com>
2025-11-06 21:07:15 -0600

    Created BookQuestions Model

---
+++ HP Trivia/Models/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  HP Trivia
+//
+//  Created by Alex Smith on 11/6/25.
+//
+
+import Foundation
+
+struct Book: Identifiable {
+    let id: Int
+    let image: String
+    let questions: [Question]
+    var status: BookStatus
+    
+    enum BookStatus {
+        case active, inactive, locked
+    }
+}
