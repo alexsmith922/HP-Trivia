36946642f10e0c4e01eef26d2b07c3bb3d95317f
alexsmith922 <10asmith@gmail.com>
2025-12-07 06:28:54 -0600

    Created persistent scores

--- HP Trivia/ViewModels/Game.swift
+++ HP Trivia/ViewModels/Game.swift
@@ -20,6 +20,12 @@ class Game {
     var currentQuestion: Question = try! JSONDecoder().decode([Question].self, from: Data(contentsOf: Bundle.main.url(forResource: "trivia", withExtension: "json")!))[0]
     var answers: [String] = []
     
+    let savePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appending(path: "RecentScores")
+    
+    init() {
+        loadScores()
+    }
+    
     func startGame() {
         for book in bookQuestions.books {
             if book.status == .active {
@@ -66,9 +72,26 @@ class Game {
         recentScores[2] = recentScores[1]
         recentScores[1] = recentScores[0]
         recentScores[0] = gameScore
-        
+        saveScores()
         gameScore = 0
         activeQuestions = []
         answeredQuestions = []
+    }
+    func saveScores() {
+        do {
+            let data = try JSONEncoder().encode(recentScores)
+            try data.write(to: savePath)
+        } catch {
+            print("Unable to save data: \(error)")
+        }
+    }
+    
+    func loadScores() {
+        do {
+            let data = try Data(contentsOf: savePath)
+            recentScores = try JSONDecoder().decode([Int].self, from: data)
+        } catch {
+            recentScores = [0,0,0]
+        }
     }
 }
