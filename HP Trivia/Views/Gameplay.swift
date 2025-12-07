a0fe0521dceebc76954d3d887fed1a69aac49186
alexsmith922 <10asmith@gmail.com>
2025-12-06 20:41:48 -0600

    Added next question animation and logic

--- HP Trivia/Views/Gameplay.swift
+++ HP Trivia/Views/Gameplay.swift
@@ -62,7 +62,7 @@ struct Gameplay: View {
                                     .transition(.scale)
                             }
                         }
-                        .animation(.easeInOut(duration: 2), value: animateViewsIn)
+                        .animation(.easeInOut(duration: animateViewsIn ? 2 : 0), value: animateViewsIn)
                         
                         Spacer()
                         
@@ -104,7 +104,7 @@ struct Gameplay: View {
                                         }
                                 }
                             }
-                            .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
+                            .animation(.easeOut(duration: animateViewsIn ? 1.5 : 0).delay(animateViewsIn ? 2 : 0), value: animateViewsIn)
                             
                             Spacer()
                             
@@ -151,7 +151,7 @@ struct Gameplay: View {
                                         }
                                 }
                             }
-                            .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
+                            .animation(.easeOut(duration: animateViewsIn ? 1.5 : 0).delay(animateViewsIn ? 2 : 0), value: animateViewsIn)
                             
                         }
                         .padding()
@@ -188,7 +188,7 @@ struct Gameplay: View {
                                             }
                                         }
                                     }
-                                    .animation(.easeInOut(duration: 1).delay(1.5), value: animateViewsIn)
+                                    .animation(.easeInOut(duration: animateViewsIn ? 1 : 0).delay(animateViewsIn ? 1.5 : 0), value: animateViewsIn)
                                 } else {
                                     VStack {
                                         if animateViewsIn {
@@ -216,7 +216,7 @@ struct Gameplay: View {
                                             .disabled(wrongAnswerstapped.contains(answer))
                                         }
                                     }
-                                    .animation(.easeInOut(duration: 1).delay(1.5), value: animateViewsIn)
+                                    .animation(.easeInOut(duration: animateViewsIn ? 1 : 0).delay(animateViewsIn ? 1.5 : 0), value: animateViewsIn)
                                 }
                             }
                         }
@@ -256,7 +256,7 @@ struct Gameplay: View {
                                 .transition(.scale.combined(with: .offset( y: -geo.size.height/2)))
                         }
                     }
-                    .animation(.easeInOut(duration: 1).delay(1), value: tappedCorrectAnswer)
+                    .animation(.easeInOut(duration: tappedCorrectAnswer ? 1 : 0).delay(tappedCorrectAnswer ? 1 : 0), value: tappedCorrectAnswer)
                     
                     if tappedCorrectAnswer {
                         Text(game.currentQuestion.answer)
@@ -275,7 +275,17 @@ struct Gameplay: View {
                     VStack {
                         if tappedCorrectAnswer {
                             Button("Next Level>") {
+                                animateViewsIn = false
+                                revealHint = false
+                                revealBook = false
+                                tappedCorrectAnswer = false
+                                wrongAnswerstapped = []
+                                movePointsToScore = false
+                                game.newQuestion()
                                 
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    animateViewsIn = true
+                                }
                             }
                             .font(.largeTitle)
                             .buttonStyle(.borderedProminent)
@@ -288,7 +298,7 @@ struct Gameplay: View {
                             }
                         }
                     }
-                    .animation(.easeInOut(duration: 2.7).delay(2.7), value: tappedCorrectAnswer)
+                    .animation(.easeInOut(duration: tappedCorrectAnswer ? 2.7 : 0).delay(tappedCorrectAnswer ? 2.7 : 0), value: tappedCorrectAnswer)
                     Spacer()
                 }
             }
